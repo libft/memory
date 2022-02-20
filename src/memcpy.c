e3f5ff52e1f1d012ad0b67d449adfb2cf7adf630
맹주영 <mjy9088@naver.com>
2022-02-20 18:16:11 +0900

    add ft_memcpy

--- src/memcpy.c
+++ src/memcpy.c
@@ -10,27 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MEMORY_H
-# define MEMORY_H
+#include <ft/memory.h>
 
-# include <stddef.h>
+void	ft_memcpy(void *dest, void *src, size_t size)
+{
+	size_t	i;
 
-void	ft_zeromemory(void *ptr, size_t size);
-void	ft_memset(void *ptr, size_t size, char value);
-void	ft_array_memset(void *ptr, size_t count, size_t size, void *value);
-
-void	ft_memcpy(void *dest, void *src, size_t size);
-void	ft_memmove(void *dest, void *src, size_t size);
-
-int		ft_memcmp(void *dest, void *src, size_t size);
-
-size_t	ft_memchr(void *ptr, char value);
-void	*ft_memchp(void *ptr, char value);
-size_t	ft_memstr(void *ptr, void *value, size_t size);
-void	*ft_memstp(void *ptr, void *value, size_t size);
-size_t	ft_memrchr(void *ptr, char value);
-void	*ft_memrchp(void *ptr, char value);
-size_t	ft_memrstr(void *ptr, void *value, size_t size);
-void	*ft_memrstp(void *ptr, void *value, size_t size);
-
-#endif
+	i = 0;
+	while (i < size)
+		*((char *)dest) = *((char *)src);
+}

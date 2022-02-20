cd7d98d8c3afaf629c18a854d575f17d3e11c96b
맹주영 <mjy9088@naver.com>
2022-02-20 19:12:22 +0900

    fix typo: `headaers` to `headers`

--- build/scripts/dist.sh
+++ build/scripts/dist.sh
@@ -7,6 +7,6 @@ cp build/release/so/lib*.so dist
 cp -r src/includes dist
 
 (cd dist && zip -r output.zip lib*.a lib*.so includes)
-(cd dist/includes && zip -r ../headaers.zip -- *)
+(cd dist/includes && zip -r ../headers.zip -- *)
 (cd src && zip -r ../dist/source.zip ./*)
 rm -rf dist/includes

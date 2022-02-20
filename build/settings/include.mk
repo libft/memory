1f9ba54d7e0db9ae9770fd38e59123f494fe45c7
맹주영 <mjy9088@naver.com>
2022-02-21 01:41:08 +0900

    correct compiler name

--- build/settings/include.mk
+++ build/settings/include.mk
@@ -12,7 +12,7 @@ $(call require_v, TARGET_MODE, debug or release)
 $(call require_v, TARGET_TYPE, a or so)
 
 # common variables
-CC = gcc
+CC = clang
 SRC_DIR := $(BASE_DIR)/src
 CFLAGS := -Weverything -Wno-poison-system-directories -Werror -std=c99 -pedantic -I$(SRC_DIR)/includes -I$(BASE_DIR)/.ft_cache/dependencies/includes
 LDFLAGS := -L$(BASE_DIR)/.ft_cache/dependencies $(shell sh $(BASE_DIR)/build/scripts/get-dependencies.sh $(BASE_DIR))
